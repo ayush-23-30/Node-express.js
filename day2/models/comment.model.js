b77f35385c0f27af448c48ed5e94d46afdaf87f7
ayush-23-30 <pawar85060@gmail.com>
2024-08-29 11:00:59 +0530

    rename

--- day2/models/comment.model.js
+++ day2/models/comment.model.js
@@ -5,7 +5,7 @@ const commentSchema = mongoose.Schema({
     type : String, 
     required : true
   },
-   post: {  
+   post:{  
   type : mongoose.Schema.Types.ObjectId, 
     ref : "Post " 
   }, 
